a5c6bf19b9d0051eb3e9f49b62e05270a998ce75
Fran Diéguez <fran.dieguez@mabishu.com>
2012-10-03 13:07:29 +0200

    Improve layout of proyectos subirmemoria action
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

--- web-service/application/views/helpers/Menuadmin.php
+++ web-service/application/views/helpers/Menuadmin.php
@@ -15,7 +15,7 @@ class Application_View_Helper_Menuadmin extends Zend_View_Helper_Abstract
             $menuAdmin .= "<a class='dropdown-toggle' data-toggle=\"dropdown\" href='#'>".Zend_Registry::get('Zend_Translate')->translate('m005')."<b class=\"caret\"></b></a>";
             $menuAdmin .= "<ul class='dropdown-menu'>
                             <li><a href='/proyectos/proyectos/crearproyecto'>".Zend_Registry::get('Zend_Translate')->translate('m006')."</a></li>
-                            <li><a href='/proyectos/versiones/creaversion'>".Zend_Registry::get('Zend_Translate')->translate('m007')."</a></li>";
+                            <li><a href='/proyectos/versiones/crearversion'>".Zend_Registry::get('Zend_Translate')->translate('m007')."</a></li>";
             $menuAdmin .= "<li><a id='menu-Pag3' href='/proyectos/memorias/subirmemoria'>".Zend_Registry::get('Zend_Translate')->translate('m004')."</a></li>";
             $menuAdmin .= "<li><a href='/administracion/usuarios/gestionusuarios'>".Zend_Registry::get('Zend_Translate')->translate('m008')."</a></li>
                             <li><a href='/administracion/recursos/index'>".Zend_Registry::get('Zend_Translate')->translate('m009')."</a></li>
