c36c5b2a15a20ac6c08168b2f630a727ea2e5f74
Fran Diéguez <fran.dieguez@mabishu.com>
2012-10-03 15:29:43 +0200

    Fix wrong click-action binding for loggedin menu
    Signed-off-by: Fran Diéguez <fran.dieguez@mabishu.com>

--- web-service/application/views/helpers/Loggedin.php
+++ web-service/application/views/helpers/Loggedin.php
@@ -10,7 +10,7 @@ class Application_View_Helper_Loggedin extends Zend_View_Helper_Abstract
         $data = $auth->getStorage()->read();
         $menuLoggedIn .= "<ul class='nav secondary-nav'>
             <li class='dropdown'>
-                <a class='dropdown-toggle' href='#'>".$data['name']."</a>
+                <a class='dropdown-toggle'  data-toggle=\"dropdown\" href='#'>".$data['name']."</a>
                 <ul class='dropdown-menu'>
                     <li><a href='/usuarios/usuarios/modificardatos'>".Zend_Registry::get('Zend_Translate')->translate('m011')."</a></li>
                     <li><a href='#'>".Zend_Registry::get('Zend_Translate')->translate('m013')."</a></li>
